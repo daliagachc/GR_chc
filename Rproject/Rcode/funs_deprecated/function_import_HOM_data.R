3587cc28c7861e114f879b7049aa36ba68afd8be
AlkuinKoenig <alkuin.koenig@gmx.de>
2019-06-06 20:12:48 -0400

    MAX

--- Rproject/Rcode/funs_deprecated/function_import_HOM_data.R
+++
@@ -1,9 +0,0 @@
-import_HOM_data=function(){
-  import=fread(paste0(getwd(),"/data","/HOMs_API_CIAPI.csv"))
-  
-  HOM_data=import %>%
-    mutate(localTime= as.POSIXct(import$TIME*24*3600-24*3600,
-                                 origin='0000-01-01 00:00.00 UTC', tz="etc/GMT+4"))
-  return(HOM_data)
-}
-
--- Rproject/Rcode/funs_deprecated/function_import_SA_data.R
+++
@@ -1,7 +0,0 @@
-#preloads and format SA data
-import_SA_data=function(){
-  SA_data=fread(paste0(getwd(),"/data","/GEORGE_SA.csv")) %>%
-    mutate(Time=as.POSIXct(TimeSA,format="%d/%m/%Y %H:%M:%S",tz="etc/GMT+4"))
-  return(SA_data)
-}
-
